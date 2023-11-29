369fe69836cb25468d840306fca577587f772a74
Kevin <Ktruong@astrophysicsinc.com>
2023-11-29 01:01:52 -0800

    Added round logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -14,4 +14,33 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playersChoice, computerChoice)
+{
+    let winMsg = "You Win! " + playersChoice + " beats " + computerChoice;
+    let loseMsg = "You Lose! " + computerChoice + " beats " + playersChoice;
+
+    if(playersChoice == computerChoice)
+    {
+        console.log("TIE");
+        return playRound(playersChoice, getComputerChoice());
+        
+    }
+
+    if(playersChoice == "rock")
+    {
+        return computerChoice == "paper" ? loseMsg : winMsg;
+    }
+    else if(playersChoice == "paper")
+    {
+        return computerChoice == "scissors" ? loseMsg : winMsg;
+    }
+    else if(playersChoice == "scissors")
+    {
+        return computerChoice == "rock" ? loseMsg : winMsg;
+    }
+}
+
+
+let playersChoice = "Rock".toLowerCase();
+let computerChoice = getComputerChoice();
+console.log(playRound(playersChoice, computerChoice));
